8fe4d1259e58f5df2666103031a4d93b6d39b607
anisimovDanil <danil1998_10@mail.ru>
2022-09-27 14:42:10 +0300

    output with DOM

--- slider/main.js
+++ slider/main.js
@@ -1,78 +1,39 @@
 const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
-const imgs = document.querySelectorAll('.image');
-const dots = document.querySelectorAll('.dot');
+const imgs = document.querySelector('.image');
+const dots = document.querySelector('.dots');
+const dots_item = document.querySelectorAll('.dots');
 const wrapper = document.querySelector('.wrapper');
 let index = 0;
+let images = ['mountains', 'car', 'forest'];
 
 
-function active_slider(val) {
+function createImage(val) {
   index += val;
-  for(var i = 0; i < imgs.length; i++) {
-    imgs[i].style.display = "none";
-  }
-
-  if(index > imgs.length - 1) index = 0;
+  if(index > images.length - 1) index = 0;
   if(index < 0) index = 2;
+  imgs.innerHTML = `<img src="img/${images[index]}.jpg" alt="picture"/>`;
+  wrapper.prepend(imgs);
+  moveDots(index);
+}
 
-  imgs[index].style.display = "block";
-
-
-  for(let j = 0; j < dots.length; j++) {
-    dots[j].className = dots[j].className.replace(" active", "");
+function createDots() {
+  for(let j = 0; j < images.length; j++) {
+    let dot = document.createElement('span'); 
+	  dot.classList.add('dot');									
+	  dots.append(dot);
   }
-  dots[index].classList += " active";
 }
 
-document.addEventListener('DOMContentLoaded', active_slider(index), false);
-  
-  prev.addEventListener('click', (e) => active_slider(-1))
-  next.addEventListener('click', (e) => active_slider(+1))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const prev = document.getElementById('prev');
-const next = document.getElementById('next');
-const images = document.getElementsByClassName('image');
-//const dots = document.getElementsByClassName('dot');
-
-let index = 0;
-
-function acive_slider(i) {
-  index += i;
-  for(i = 0; i < images.length; i++) {
-    images[i].style.display = 'none';
-  }*/
-
-  /*for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }*/
-  /*if(index > images.length - 1) index = 0;
-  else if(index < 0) index = images.length - 1;
-  
-  images[index].style.display = 'block';
-  //dots[index].className += " active";
-  
+function moveDots(index) {
+  for(let j = 0; j < images.length; j++) {
+    dots.children[j].className = dots.children[j].className.replace(" active", "");
+  }
+  dots.children[index].classList += " active";
 }
 
-window.addEventListener("onload", acive_slider(index));
-prev.addEventListener('click', (e) => acive_slider(-1))
-next.addEventListener('click', (e) => acive_slider(+1))*/
+
+createDots(); 
+document.addEventListener('DOMContentLoaded', createImage(index), false);
+prev.addEventListener('click', (e) => createImage(-1))
+next.addEventListener('click', (e) => createImage(+1))
